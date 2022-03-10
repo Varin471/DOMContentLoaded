24f3d6f43ac72908aa3efc325f8afdc2b5a8a045
LC <94356640+Varin-js@users.noreply.github.com>
2022-03-10 17:55:54 +0700

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var scollpos = localStorage.getItem('scrollpos');
+    if (scollpos) window.scrollTo(0, scollpos);
+});
+
+window.onbeforeunload = function(e) {
+    localStorage.sectltem('scrollpos', window.scroll(Y));
+};
